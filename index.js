99a6e4d0aea22209859c08cb50ea093ecf0048bb
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-11-20 15:56:55 +0530

    feat:

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require("dotenv").config();
-
 const express = require("express");
 require("./configs/mongoose");
+require("dotenv").config();
 const cors = require("cors");
 
 const userRouter = require("./controller/user");
 const packageRouter = require("./controller/package");
 const guideRouter = require("./controller/guide");
 const blogRouter = require("./controller/blog");
+const orderRouter = require("./controller/order");
 
 const app = express();
 
@@ -19,6 +19,7 @@ app.use(userRouter);
 app.use(packageRouter);
 app.use(guideRouter);
 app.use(blogRouter);
+app.use(orderRouter);
 
 app.listen(port, () => {
   console.log(`running on port ${port}`);
